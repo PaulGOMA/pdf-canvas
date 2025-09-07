40847cc8d8fad839274f7ebd191ea5a5e97f3155
Paul Valerie GOMA <paulgoma07@gmail.com>
2025-09-07 20:15:01 +0200

    Add vitest dependency

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+// vitest.config.ts
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['test/**/*.spec.ts', 'test/**/*.test-d.ts'],
+  },
+})
